4e09244b471c11187e055df26ce5246d710226b0
monika1127 <dziubmonika@gmail.com>
2022-02-11 10:55:31 +0100

    setup native-base, setup react-navigation

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NativeBaseProvider, Text, Box } from "native-base";
+import Home from "./screens/Home";
+import Register from "./screens/Register";
+import Login from "./screens/Login";
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <Box>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </Box>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Register" component={Register} />
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="Home" component={Home} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
